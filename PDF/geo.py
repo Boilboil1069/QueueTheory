2745c73416bc5af5403b8fb67a8b9d658b4f71a4
Manning Cyrus <1069720030@qq.com>
2025-03-13 22:21:55 +0800

    20250313

--- PDF/geo.py
+++ PDF/geo.py
@@ -2,5 +2,13 @@ from utils import numeric
 
 
 class geo(numeric):
-    def __init__(self, rate, f: callable, var, interval: tuple):
-        super().__init__(f, var, interval)
+    def __init__(self, p: float,  var: str, interval: tuple):
+        """
+            :param p: The probability of success
+            :param var: The variable of integration
+            :param interval: The upper and lower bounds of integration
+            """
+        def geometric_pdf(i):
+            return (1 - p) ** (i - 1) * p
+
+        super().__init__(geometric_pdf, var, interval)
